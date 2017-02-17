003d8fe9df23ea72667e3e3ddc1f8a2d13b1b09c
Jeam Alves <jeanmalves@gmail.com>
2017-02-17 02:21:27 -0200

    Alteração teste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.admin_template')
 
 @section('content')
-   <example></example>
+   <!-- <example></example> -->
+   <!-- Default box -->
+   <div class="box">
+     <div class="box-body">
+       Start creating your amazing application!
+     </div>
+     <!-- /.box-body -->
+     <div class="box-footer">
+       Footer
+     </div>
+     <!-- /.box-footer-->
+   </div>
+   <!-- /.box -->
 @endsection
