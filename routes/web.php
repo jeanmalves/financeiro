b34e0730f56002745128035aa9bd2b6376203ea3
Jeam Alves <jeanmalves@gmail.com>
2017-02-18 13:10:23 -0200

    Definição de rota para o recurso conta

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,21 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+// Route::get('/admin', 'HomeController@index');
+
+Route::group(['prefix'=>'admin'], function(){
+  Route::get('contas', ['as' => 'conta.index', 'uses' => 'AccountController@index']);
+  Route::get('conta/{id}', ['as' => 'conta.get', 'uses' => 'AccountController@get']);
+  Route::post('conta', ['as' => 'conta.create', 'uses' => 'AccountController@create']);
+  Route::put('conta/{id}', ['as' => 'conta.update', 'uses' => 'AccountController@update']);
+  Route::delete('conta/{id}', ['as' => 'conta.delete', 'uses' => 'AccountController@delete']);
+});
+
+//
+// admin/contas   get   listar
+// admin/conta/1  get   recuperar
+// admin/conta    post  criar novo
+// admin/conta/1  put   atualizar
+// admin/conta/1  delete remover
+
+// cada conta a ser cadastrada tem um nome e um saldo(valor de entrada)
